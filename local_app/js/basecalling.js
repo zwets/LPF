8939ba16e5d6b81956077293837eacb47632cb8e
Malte Hallgreen <malhal@dtu-fvff80kgq05p.clients.net.dtu.dk>
2022-01-03 12:31:46 +0100

    jan3_36

--- local_app/js/basecalling.js
+++ local_app/js/basecalling.js
@@ -127,7 +127,8 @@ function find_model_from_input(flowcell, kit, db_dir, algorithm){
             if (data[i].flowcell == flowcell) {
                 if (data[i].kit == kit) {
                     model = data[i].barcoding_config_name;
-                    model = model[:-4] + algorithm;
+                    model = model[:-4];
+                    model = model.concat(`${algorithm}`)
                     console.log(model);
                     //return model
                 };
