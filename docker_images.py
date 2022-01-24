46d2aa7c5c08f4a08b8004c33b5d6c4937b66116
Malte Hallgreen <malhal@DTU-FVFF80KGQ05P.local>
2022-01-24 07:48:02 +0100

    trusted guppy

--- docker_images.py
+++ docker_images.py
@@ -1,5 +1,5 @@
 import os
 import sys
 #docker pull nanozoo/bandage
-cmd = "docker pull biocontainers/figtree:v1.4.4-3-deb_cv1; docker pull nanozoo/unicycler:0.4.7-0--c0404e6; docker images; docker pull staphb/quast"
+cmd = "docker pull biocontainers/figtree:v1.4.4-3-deb_cv1; docker pull nanozoo/unicycler:0.4.7-0--c0404e6; docker images; docker pull staphb/quast;"
 os.system(cmd)
--- ubuntu_install.py
+++ ubuntu_install.py
@@ -25,7 +25,7 @@ def main():
     sudo apt-get install wget lsb-release
     export PLATFORM=$(lsb_release -cs)
     wget -O- https://mirror.oxfordnanoportal.com/apt/ont-repo.pub | sudo apt-key add -
-    echo "deb http://mirror.oxfordnanoportal.com/apt ${PLATFORM}-stable non-free" | sudo tee /etc/apt/sources.list.d/nanoporetech.sources.list
+    echo "deb [trusted=yes] http://mirror.oxfordnanoportal.com/apt ${PLATFORM}-stable non-free" | sudo tee /etc/apt/sources.list.d/nanoporetech.sources.list
     sudo apt-get update
     sudo apt update
     sudo apt install ont-guppy
