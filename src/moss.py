24b3d929a6cb6acceb5ffa3f0ee4545034a22266
Malte Hallgreen <malhal@DTU-FVFF80KGQ05P.local>
2022-01-27 13:19:04 +0100

    27_1_8

--- src/moss.py
+++ src/moss.py
@@ -190,7 +190,7 @@ def moss_pipeline(seqType, prune_distance, bc,
     #cmd = "cp {}{}_{}_consensus.fsa {}datafiles/isolatefiles/{}/{}_{}_consensus.fsa".format(target_dir, samplename, templateaccesion, db_dir, templateaccesion, samplename, templateaccesion)
     #os.system(cmd)
 
-    related_isolates = fetch_isolates(db_dir, header_text)
+    related_isolates = moss.fetch_isolates(db_dir, header_text)
     print (related_isolates)
 
     sys.exit("Pre ccphylo")
