5480987b39d15d96ea981fe80d7abd81d18bd795
Malte Hallgreen <malhal@DTU-FVFF80KGQ05P.local>
2022-01-27 15:30:24 +0100

    27_1_16

--- src/moss_functions.py
+++ src/moss_functions.py
@@ -62,7 +62,7 @@ def make_phytree_output_folder(db_dir, target_dir, isolate_list, exepath, header
         cmd = "cp {} {}/phytree_output/.".format(path, target_dir)
         os.system(cmd)
     number = get_kma_template_number(header_text, db_dir)
-    header_name = header_text.split()[0][1:]
+    header_name = header_text.split()[0]
     cmd = "{}/kma/kma seq2fasta -t_db {}REFDB.ATG -seqs {} > {}/phytree_output/{}.fsa".format(exepath, db_dir, number, target_dir, header_name)
     os.system(cmd)
 
