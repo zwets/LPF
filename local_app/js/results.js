73de8f8cade1cc45ab54e8a87859c5f2bda7acf5
Malte Hallgreen <malhal@dtu-fvff80kgq05p.clients.net.dtu.dk>
2022-02-09 11:08:42 +0100

    feb9_15

--- local_app/js/results.js
+++ local_app/js/results.js
@@ -68,6 +68,7 @@ function showFinishedAnalyses() {
               if (error) throw error;
 
               let sql = `SELECT * FROM statustable`;
+              document.getElementById('showData').innerHTML="" ;
                 let db_dir = document.getElementById('current-config').innerHTML
                 const db = require('better-sqlite3')(db_dir + 'moss.db');
                 const sql_data_obj = db.prepare(sql).all();
@@ -78,110 +79,12 @@ function showFinishedAnalyses() {
 
 }
 
-function showRunningAnalyses() {
-    storage.get('currentConfig', function(error, data) {
-          if (error) throw error;
-
-          tableFromObj('runningAnalyses.json', 'none');
-    });
-}
-
-function showQueuedAnalyses() {
-    storage.get('currentConfig', function(error, data) {
-      if (error) throw error;
-
-      tableFromObj('queuedAnalyses.json', 'none');
-    });
-}
-
 function openPDF(id, data){
   console.log(data.db_dir + "analysis/" + document.getElementById(id).name + "/" + id + "_report.pdf");
   window.open(data.db_dir + "analysis/" + document.getElementById(id).name + "/" + id + "_report.pdf");
   //return false;
 }
 
-function most_recent_isolates_table(data_obj, data) {
-        var divShowData = document.getElementById('showData2');
-        divShowData.innerHTML = "";
-		var myObject = [];
-        var col = [];
-        col.push('entryid');
-        col.push('samplename');
-        col.push('status');
-        col.push('type');
-        col.push('current_stage');
-        col.push('final_stage');
-        col.push('result');
-
-        // Create a table.
-        var table = document.createElement("table");
-        table.innerHTML = "";
-
-        // Create table header row using the extracted headers above.
-        var tr = table.insertRow(-1);                   // table row.
-
-        for (var i = 0; i < col.length; i++) {
-            var th = document.createElement("th");      // table header.
-            th.innerHTML = col[i];
-            tr.appendChild(th);
-        }
-
-        // add json data to the table as rows.
-        for (var i = 0; i < data_obj.length; i++) {
-            let db_dir = document.getElementById('current-config').innerHTML
-            const db = require('better-sqlite3')(db_dir + 'moss.db');
-            //WHERE ENTRYID IN TABLE, CHANGE SQL STRUCTURE
-
-            sql = `SELECT samplename FROM isolatetable WHERE entryid = '${data_obj[i].entryid}'`;
-            console.log(sql);
-            var name = db.prepare(`sql`).all()[0].samplename;
-            console.log(name);
-
-
-
-            tr = table.insertRow(-1);
-
-            var tabCell = tr.insertCell(-1);
-            tabCell.innerHTML = data_obj[i].entryid;
-            var tabCell = tr.insertCell(-1);
-            tabCell.innerHTML = name;
-            var tabCell = tr.insertCell(-1);
-            tabCell.innerHTML = data_obj[i].status;
-            var tabCell = tr.insertCell(-1);
-            tabCell.innerHTML = data_obj[i].type;
-            var tabCell = tr.insertCell(-1);
-            tabCell.innerHTML = data_obj[i].current_stage;
-            var tabCell = tr.insertCell(-1);
-            tabCell.innerHTML = data_obj[i].final_stage;
-            var tabCell = tr.insertCell(-1);
-            tabCell.innerHTML = data_obj[i].result;
-
-            if (tabCell.innerHTML == "Finished") {
-                var tabCell = tr.insertCell(-1);
-                var img = data[i].entryid;
-                img.id = data[i].entryid;
-                img.name = data[i].samplename;
-                img.src = data.exepath + "local_app/images/report-icon.png";
-                img.setAttribute('height', '17pt');
-                img.innerHTML = data_obj[i].entryid;
-                img.onclick = function() {openPDF(this.id, data)};
-                //el.addEventListener("click", function(){
-                //    openPDF(Object.keys(myjson)[i]));
-                //});
-                tabCell.appendChild(img);
-
-            }
-
-
-
-
-        }
-        table.style.border = "thin solid red";
-        table.style.cssFloat = "center";
-        table.style.padding = "1px 10px 10px 10px";
-        divShowData.appendChild(table);
-
-    }
 
 function showanalyses() {
     let sql = `SELECT * FROM statustable`;
