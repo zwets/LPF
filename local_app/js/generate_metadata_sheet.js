5daf1a622ab6fa8b2340e116947661bc16f94be4
MBHallgren <s153002@student.dtu.dk>
2022-04-11 10:39:29 +0200

    ux_11_4_31

--- local_app/js/generate_metadata_sheet.js
+++ local_app/js/generate_metadata_sheet.js
@@ -52,7 +52,7 @@ function create_metadata_table(){
       var header_row = rows[0];
       console.log(header_row.cells.length);
       for (var i = 0; i < header_row.cells.length; i++) {
-          csv_string = csv_string.concat(`header_row.cells[i], `);
+          csv_string = csv_string.concat(`${header_row.cells[i]}, `);
         }
       csv_string.concat(`file_location\n`);
 
