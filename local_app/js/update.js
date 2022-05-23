b289c4a32ec94a289810d761197e308d2e1b3fdd
MBHallgren <s153002@student.dtu.dk>
2022-05-23 10:38:50 +0200

    light install55

--- local_app/js/update.js
+++ local_app/js/update.js
@@ -4,7 +4,10 @@ const storage = require('electron-json-storage');
 
 function update_moss(){
     var current_moss_system = require('/opt/moss_db/config.json')["current_working_db"] + "/";
-    var cmd = `conda run -n base python3 /opt/moss/moss_install.py -light -i`
+    var cmd = `conda run -n base python3 /opt/moss/moss_install.py -light`;
+    var loader = document.getElementById('loader');
+    loader.style.display = 'block';
+    document.getElementById('loadermessage').innerHTML = "Basecalling is running";
     exec(cmd, (error, stdout, stderr) => {
 
             if (error) {
