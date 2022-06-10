e47a570a33fce853628e6c62883b590a0f3bd2f4
MBHallgren <s153002@student.dtu.dk>
2022-06-10 13:20:00 +0200

    22_install5

--- src/createGuppyWorkflowDict.py
+++ src/createGuppyWorkflowDict.py
@@ -8,7 +8,7 @@ parser = argparse.ArgumentParser(description='MinION-Typer-2.0')
 parser.add_argument("-current_working_db", action="store", dest="current_working_db", help="Enter a name for your configuration file.")
 args = parser.parse_args()
 
-cmd = "/opt/moss/ont-guppy/bin/./guppy_basecaller --print_workflows > /opt/moss_db/{}/static_files/tmpworkflowdict.txt".format(args.current_working_db)
+cmd = "/opt/ont/guppy/bin/guppy_basecaller --print_workflows > /opt/moss_db/{}/static_files/tmpworkflowdict.txt".format(args.current_working_db)
 os.system(cmd)
 
 infile = open("/opt/moss_db/{}/static_files/tmpworkflowdict.txt".format(args.current_working_db), 'r')
@@ -37,7 +37,7 @@ print (json.dumps(jsonlist, indent=2), file=outfile)
 outfile.close()
 
 
-cmd = "/opt/moss/ont-guppy/bin/./guppy_barcoder --print_kits > /opt/moss_db/{}/static_files/printkitstmp.txt".format(args.current_working_db)
+cmd = "/opt/ont/guppy/bin/guppy_barcoder --print_kits > /opt/moss_db/{}/static_files/printkitstmp.txt".format(args.current_working_db)
 os.system(cmd)
 
 infile = open("/opt/moss_db/{}/static_files/printkitstmp.txt".format(args.current_working_db), 'r')
