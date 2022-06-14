cf79d18d9e8e9d868c0972a6bcc039bbb962fff9
MBHallgren <s153002@student.dtu.dk>
2022-06-14 20:43:18 +0200

    emmit4

--- local_app/js/initdb.js
+++ local_app/js/initdb.js
@@ -1,6 +1,9 @@
-const { exec } = require('child_process');
-const fs = require('fs')
-const storage = require('electron-json-storage');
+//const { exec } = require('child_process');
+import exec from 'child_process';
+//const fs = require('fs')
+import fs from 'fs';
+//const storage = require('electron-json-storage');
+import storage from 'electron-json-storage';
 
 function getfolder(id) {
     var trypath = document.getElementById(id).files[0].path;
