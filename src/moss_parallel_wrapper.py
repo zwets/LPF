4ac32c5804571ed2a088e1f8aec11160e7cc1a65
MBHallgren <s153002@student.dtu.dk>
2022-08-30 10:57:57 +0200

    test2

--- src/moss_parallel_wrapper.py
+++ src/moss_parallel_wrapper.py
@@ -10,40 +10,34 @@ parser = argparse.ArgumentParser(description='.')
 parser.add_argument('-info', type=int, help='surveillance info')
 parser.add_argument('-json', action="store", type=str, dest='json', default="",
                     help='json file')
-parser.add_argument('-config_name', action="store", type=str, dest='config_name',
-                    default="", help='config_name')
 args = parser.parse_args()
 
 def moss_analysis(jobslist, i):
     """Start analysis"""
     os.system(jobslist[i])
 
-def main(json_file, config_name):
+def main(json_file):
     """Main func"""
     with open(json_file) as infile:
         data = json.load(infile)
-    print (data)
-    for item in data['samples']:
-        print (item)
-    sys.exit("Printed json data")
     jobslist = []
 
-    for i in range(len(metadata_list)):
-        cmd = "python3 /opt/moss/src/moss.py -config_name {} -metadata \"{}\"" \
-              " -metadata_headers \"{}\"".format(config_name, metadata_list[i], metadata_headers)
+    for item in data['samples']:
+        cmd = 'python3 /opt/moss/src/moss.py -json {}'.format(item)
         jobslist.append(cmd)
-        input_file = metadata_list[i].split(",")[-2]
-        sample_name = metadata_list[i].split(",")[0]
-        entry_id = moss.md5_of_file(input_file)
+        entry_id = moss.md5_of_file(item['input_path'])
         moss.sql_execute_command(
             "INSERT INTO status_table(entry_id, sample_name, status,"
             " type, current_stage, final_stage, result, time_stamp)"
             " VALUES('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}')" \
-            .format(entry_id, sample_name, "Queued", "Queued", "Queued",
-                    "Queued", "Queued", ""), config_name)
+            .format(entry_id, item['sample_name'], "Queued", "Queued", "Queued",
+                    "Queued", "Queued", ""), item['config_path'] + 'moss.db')
+    print (jobslist)
+    sys.exit()
+
 
     Parallel(n_jobs=jobs)(delayed(moss_analysis)(jobslist, i) for i in range(len(jobslist)))
     print ("Analysis complete")
 
 if __name__== "__main__":
-    main(args.json, args.config_name)
+    main(args.json)
