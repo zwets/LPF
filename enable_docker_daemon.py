4f03d173e08dfc889e5cc27a7676d8144a549569
MBHallgren <s153002@student.dtu.dk>
2022-09-06 12:35:20 +0200

    p7

--- enable_docker_daemon.py
+++
@@ -1,4 +0,0 @@
-import os
-
-os.system('sudo groupadd docker; sudo usermod -aG docker $USER; su -s ${USER}; newgrp docker;')
-print ('Please restart the terminal and confirm the docker installation my running the docker images command and check for no errors')
