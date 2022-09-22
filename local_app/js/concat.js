f3da3e647902dedba6d0a0fab6cd163d9d542dc5
MBHallgren <s153002@student.dtu.dk>
2022-09-22 10:39:30 +0200

    concat9

--- local_app/js/concat.js
+++ local_app/js/concat.js
@@ -15,13 +15,14 @@ function merge_fastq() {
   var current_moss_system = require('/opt/moss_db/config.json')["current_working_db"] + "/";
   var folder = getfolder("fastq_folder_path");
   var execstring = `~/anaconda3/bin/conda run -n base python3 /opt/moss/src/moss_merge_fastq.py -folder ${folder} -name ${fastq_name}`;
-  /*
+
   console.log(execstring);
     console.log("job submitted");
     alert("job submitted.");
     var loader = document.getElementById('loader');
     loader.style.display = 'block';
     document.getElementById('loadermessage').innerHTML = "Merging fastq files. This might take some time, if there are a lot of files.";
+    /*
     exec(execstring, (error, stdout, stderr) => {
         if (error) {
           //If error, change accepted ui to failure, which attached message.
