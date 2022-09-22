35b3a9420f59b6556a21f203a8e67ab31102ca70
MBHallgren <s153002@student.dtu.dk>
2022-09-22 10:48:34 +0200

    concat15

--- src/moss_merge_fastq.py
+++ src/moss_merge_fastq.py
@@ -10,7 +10,7 @@ parser.add_argument("-name", action="store", type=str, default = "", dest="name"
 args = parser.parse_args()
 
 def merge():
-    existing_list = os.path.listdir("/opt/moss_data/")
+    existing_list = os.listdir("/opt/moss_data/")
     if args.name in existing_list:
         sys.exit('There is already a file with that name in the moss_data folder. Please choose a different name.')
     cmd = 'cat {}*.fastq.gz > /opt/moss_data/{}.fastq.gz'.format(args.folder, args.name)
