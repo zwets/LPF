aeae5d14867ec94f41c39cb5d37b5c84c1e2dca1
MBHallgren <s153002@student.dtu.dk>
2022-11-14 13:22:13 +0100

    guppy removed docker

--- docker_images.py
+++ docker_images.py
@@ -3,7 +3,6 @@ cmd = "docker pull biocontainers/figtree:v1.4.4-3-deb_cv1;\
     docker pull nanozoo/unicycler:0.4.7-0--c0404e6;\
     docker pull staphb/quast;\
     docker pull staphb/flye;\
-    docker pull genomicpariscentre/guppy-gpu;\
     docker pull nanozoo/bandage;\
     docker pull ontresearch/medaka"
 os.system(cmd)
