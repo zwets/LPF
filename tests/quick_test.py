053fa5625bfe7b247d7348c5eea7088328be076b
MBHallgren <s153002@student.dtu.dk>
2022-11-23 13:25:27 +0100

    object39

---
+++ tests/quick_test.py
@@ -0,0 +1,5 @@
+import os
+os.system("sudo rm -rf /opt/moss_db/test")
+os.system("sudo git pull")
+os.system("python3 /opt/moss/src/moss_init.py -db /opt/moss/tests/fixtures/data_for_tests/database/ -config test")
+os.system("python /opt/moss/src/moss_parallel.py -json /opt/moss/tests/fixtures/data_for_tests/json/alignment_test.json")
