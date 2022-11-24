9218dc68677e86e574593381f83d95dbb0979cd6
MBHallgren <s153002@student.dtu.dk>
2022-11-24 15:49:36 +0100

    0.2.1

--- src/validate_build.py
+++
@@ -1,8 +0,0 @@
-"""Tests and validates a moss installation"""
-
-def check_dependencies():
-    """Validates specific set of dependencies for the MOSS installation"""
-    return True
-
-if __name__ == '__main__':
-    check_dependencies()
