6b1bc7f5df7b49aa5d7297d5230117b4cbf1f985
MBHallgren <s153002@student.dtu.dk>
2023-01-31 12:07:17 +0100

    31/1 3

--- tests/quick_test.py
+++ tests/quick_test.py
@@ -1,9 +1,10 @@
 import os
-
+import src.sqlCommands as sqlCommands
 def clean_up(md5_list):
     for item in md5_list:
         os.system("sudo rm -rf /opt/LPF_logs/{}.log".format(item))
         os.system("sudo rm -rf /opt/LPF_analyses/{}".format(item))
+        sqlCommands.sql_execute_command('DELETE FROM status_table WHERE entry_id = "{}"'.format(item))
 
 if __name__ == '__main__':
     md5_list = ['62b06be200d3967db6b0f6023d7b5b2e', 'fac82762aa980d285edbbcd45ce952fb']
