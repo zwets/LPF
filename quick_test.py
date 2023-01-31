9911bd5ba12c6781289a246fcd9b5f2178a870ba
MBHallgren <s153002@student.dtu.dk>
2023-01-31 13:42:38 +0100

    31/1 8

--- quick_test.py
+++ quick_test.py
@@ -4,11 +4,11 @@ def clean_up(md5_list):
     for item in md5_list:
         os.system("sudo rm -rf /opt/LPF_logs/{}.log".format(item))
         os.system("sudo rm -rf /opt/LPF_analyses/{}".format(item))
-        sqlCommands.sql_execute_command('DELETE FROM status_table WHERE entry_id = "{}"'.format(item))
-        sqlCommands.sql_execute_command('DELETE FROM meta_data_table WHERE entry_id = "{}"'.format(item))
-        sqlCommands.sql_execute_command('DELETE FROM bacteria_table WHERE entry_id = "{}"'.format(item))
-        sqlCommands.sql_execute_command('DELETE FROM bacteria_reference_table WHERE entry_id = "{}"'.format(item))
-        sqlCommands.sql_execute_command('DELETE FROM sample_table WHERE entry_id = "{}"'.format(item))
+        sqlCommands.sql_execute_command('DELETE FROM status_table WHERE entry_id = "{}"'.format(item), '/opt/LPF_databases/LPF.db')
+        sqlCommands.sql_execute_command('DELETE FROM meta_data_table WHERE entry_id = "{}"'.format(item), '/opt/LPF_databases/LPF.db')
+        sqlCommands.sql_execute_command('DELETE FROM bacteria_table WHERE entry_id = "{}"'.format(item), '/opt/LPF_databases/LPF.db')
+        sqlCommands.sql_execute_command('DELETE FROM bacteria_reference_table WHERE entry_id = "{}"'.format(item), '/opt/LPF_databases/LPF.db')
+        sqlCommands.sql_execute_command('DELETE FROM sample_table WHERE entry_id = "{}"'.format(item), '/opt/LPF_databases/LPF.db')
 
 
 if __name__ == '__main__':
