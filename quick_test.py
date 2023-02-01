4f06747ddea2fabe08b798d919e4bb686d781d8b
MBHallgren <s153002@student.dtu.dk>
2023-02-01 10:30:45 +0100

    11 14

--- quick_test.py
+++ quick_test.py
@@ -6,10 +6,8 @@ def clean_up(md5_list):
         os.system("sudo rm -rf /opt/LPF_analyses/{}".format(item))
         sqlCommands.sql_execute_command('DELETE FROM status_table WHERE entry_id = \"{}\"'.format(item), '/opt/LPF_databases/LPF.db')
         sqlCommands.sql_execute_command('DELETE FROM meta_data_table WHERE entry_id = \"{}\"'.format(item), '/opt/LPF_databases/LPF.db')
-        sqlCommands.sql_execute_command('DELETE FROM bacteria_table WHERE entry_id = \"{}\"'.format(item), '/opt/LPF_databases/LPF.db')
-        sqlCommands.sql_execute_command('DELETE FROM bacteria_reference_table WHERE entry_id = \"{}\"'.format(item), '/opt/LPF_databases/LPF.db')
         sqlCommands.sql_execute_command('DELETE FROM sample_table WHERE entry_id = \"{}\"'.format(item), '/opt/LPF_databases/LPF.db')
-
+        sqlCommands.sql_execute_command('DELETE FROM sequence_table WHERE entry_id = \"{}\"'.format(item), '/opt/LPF_databases/LPF.db')
 
 if __name__ == '__main__':
     md5_list = ['62b06be200d3967db6b0f6023d7b5b2e', 'fac82762aa980d285edbbcd45ce952fb']
