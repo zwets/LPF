f5f64d3e15ba958fc84eeac19cba0646fa1c5d86
MBHallgren <s153002@student.dtu.dk>
2023-02-07 13:23:09 +0100

    add_to_kma_database 15

--- src/util/cleanFailedRun.py
+++ src/util/cleanFailedRun.py
@@ -1,4 +1,5 @@
 import os
 import src.sqlCommands as sqlCommands
 def clean_failed_run(arguments):
-    sqlCommands.sql_update_status_table('Analysis failed', arguments.sample_name, 'Failed, log info in /opt/LPF_logs', arguments.entry_id, arguments.sql_db)
+    """Clean up failed run"""
+    sqlCommands.sql_update_status_table('Analysis failed', arguments.input_file, 'Failed, log info in /opt/LPF_logs', arguments.entry_id, arguments.sql_db)
