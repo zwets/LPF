bf3fd90dc66571a0b59def0a043f34aff09b1fb4
MBHallgren <s153002@student.dtu.dk>
2023-02-07 13:53:33 +0100

    add_to_kma_database 17

--- src/util/cleanFailedRun.py
+++ src/util/cleanFailedRun.py
@@ -1,5 +1,16 @@
 import os
 import src.sqlCommands as sqlCommands
-def clean_failed_run(arguments):
+import json
+import src.util.md5 as md5
+
+class EmptyDataObject:
+    pass
+def clean_failed_run(json):
     """Clean up failed run"""
-    sqlCommands.sql_update_status_table('Analysis failed', arguments.input_file, 'Failed, log info in /opt/LPF_logs', arguments.entry_id, arguments.sql_db)
+    with open(json, 'r') as f:
+        data_dict = json.loads(f.read())
+    data = EmptyDataObject()
+    for item in data_dict:
+        setattr(data, item, data_dict[item])
+    data.entry_id = md5.md5_of_file(data.input_path)
+    sqlCommands.sql_update_status_table('Analysis failed', data.input_file, 'Failed, log info in /opt/LPF_logs', data.entry_id, data.sql_db)
