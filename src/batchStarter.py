b081fe9b65cc28791b5a2798e0a4f55c73899815
MBHallgren <s153002@student.dtu.dk>
2023-02-07 12:16:53 +0100

    add_to_kma_database 11

--- src/batchStarter.py
+++ src/batchStarter.py
@@ -38,7 +38,7 @@ def main(analysis_type, batch_json):
         except Exception as e:
             sys.exit("LocalPathogenFinder: Error: {}. LPF was NOT run.".format(e))
 
-        if os.path.exists('/opt/LPF/LPF'):
+        if os.path.exists('/opt/LPF/LocalPathogenFinder'):
             cmd = 'python3 /opt/LPF/LocalPathogenFinder {} -json {}'.format(analysis_type, item)
         else:
             cmd = 'python3 LocalPathogenFinder {} -json {}'.format(analysis_type, item)
