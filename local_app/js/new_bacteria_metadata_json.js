72b881e67b70297506ca9a83676520a0420084c9
MBHallgren <s153002@student.dtu.dk>
2023-02-08 09:37:03 +0100

    select tag15

--- local_app/js/new_bacteria_metadata_json.js
+++ local_app/js/new_bacteria_metadata_json.js
@@ -71,6 +71,7 @@ function create_meta_data_table_fastq(){
             for (let t = 0; t < rows[i].cells.length; t++) {
                 if (header_row.cells[t].innerHTML == 'input_file') {
                     console.log(document.getElementById(`${header_row.cells[t].innerHTML}${i}`));
+                    console.log(document.getElementById(`${header_row.cells[t].innerHTML}${i}`).value);
                     new_obj[header_row.cells[t].innerHTML] = document.getElementById(`${header_row.cells[t].innerHTML}${i}`).value[0];
                 } else {
                     new_obj[header_row.cells[t].innerHTML] = document.getElementById(`${header_row.cells[t].innerHTML}${i}`).value;
