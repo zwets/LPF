d57d4c789ead240d596f1ac94fe088135d45ddcc
MBHallgren <s153002@student.dtu.dk>
2023-02-08 09:40:36 +0100

    select tag19

--- local_app/js/new_bacteria_metadata_json.js
+++ local_app/js/new_bacteria_metadata_json.js
@@ -70,11 +70,9 @@ function create_meta_data_table_fastq(){
             let new_obj = {};
             for (let t = 0; t < rows[i].cells.length; t++) {
                 if (header_row.cells[t].innerHTML == 'input_file') {
-                    console.log(document.getElementById(`input${t}${i}`));
-                    console.log(document.getElementById(`input${t}${i}`).value);
-                    new_obj[header_row.cells[t].innerHTML] = document.getElementById(`input${header_row.cells[t].innerHTML}${i}`).value[0];
+                    new_obj[header_row.cells[t].innerHTML] = document.getElementById(`input${t}${i}`).value[0];
                 } else {
-                    new_obj[header_row.cells[t].innerHTML] = document.getElementById(`input${header_row.cells[t].innerHTML}${i}`).value;
+                    new_obj[header_row.cells[t].innerHTML] = document.getElementById(`input${t}${i}`).value;
                 }
             }
             new_obj['input_path'] = file_list_obj[i].path;
