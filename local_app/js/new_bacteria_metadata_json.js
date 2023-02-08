67c78a72ab38f99537d7257657a6f3f461fc6279
MBHallgren <s153002@student.dtu.dk>
2023-02-08 09:29:11 +0100

    select tag12

--- local_app/js/new_bacteria_metadata_json.js
+++ local_app/js/new_bacteria_metadata_json.js
@@ -263,20 +263,18 @@ function getCities(rowNumber, columnNumber) {
 }
 
 function getCustomValue(i, j) {
-    const value = document.getElementById(`input${[i]}${[j]}`).value;
-    if (value == "Custom city") {
+    const element = document.getElementById(`input${[i]}${[j]}`);
+    if (element.value == "Custom city") {
         const Dialogs = require('dialogs')
         const dialogs = Dialogs()
         dialogs.prompt('Give the city name', ok => {
             console.log(ok);
-            /*
             const custom_option = document.createElement("option");
             custom_option.value = ok;
             custom_option.text = ok;
-            document.getElementById(`input${[i]}${[t]}`).add(custom_option);
-            document.getElementById(`input${[i]}${[t]}`).value = custom_option.value;
-
-             */
+            element.add(custom_option);
+            element.value = custom_option.value;
+            console.log(element.value);
         })
     }
 }
