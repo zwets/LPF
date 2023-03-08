fdf6b39cfa02598237b361031cff9ad5131078d4
MBHallgren <s153002@student.dtu.dk>
2023-03-08 11:07:11 +0100

    js report

--- src/JSreport.py
+++ src/JSreport.py
@@ -4,12 +4,14 @@ import argparse
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-assembly', action="store_true", type=bool, default=False, dest="assembly")
-    parser.add_argument('-alignment', action="store_true", type=bool, default=False, dest="alignment")')
-
+    parser.add_argument('-alignment', action="store_true", type=bool, default=False, dest="alignment")
+    parser.add_argument('-id ', action="store", type=str, default="", dest="id")
     args = parser.parse_args()
     eval_res, js_file = js2py.run_file("createPDF.js")
-    js_file.generatePDFReport(args.target_dir)
+    if args.assembly:
+        js_file.generatePDFReport(args.id, 'assembly')
+    elif args.alignment:
+        js_file.generatePDFReport(args.id, 'alignment')
 
-def generate_assembly_report(bacterial_parser):
-    eval_res, js_file = js2py.run_file("createPDF.js") #to run the js file
-    js_file.generatePDFReport(bacterial_parser.data.target_dir) #bacterial_parser.data.target_dir is the id to call the function
+if __name__ == "__main__":
+    main()
