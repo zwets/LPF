79478a94725a293312df9406c232d3112d77b18b
MBHallgren <s153002@student.dtu.dk>
2023-03-08 11:09:57 +0100

    js report3

--- src/JSreport.py
+++ src/JSreport.py
@@ -3,9 +3,9 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-assembly', action="store_true", type=bool, default=False, dest="assembly")
-    parser.add_argument('-alignment', action="store_true", type=bool, default=False, dest="alignment")
-    parser.add_argument('-id ', action="store", type=str, default="", dest="id")
+    parser.add_argument('-assembly', action="store_true", default=False, dest="assembly")
+    parser.add_argument('-alignment', action="store_true", default=False, dest="alignment")
+    parser.add_argument('-id ', action="store", default="", dest="id")
     args = parser.parse_args()
     eval_res, js_file = js2py.run_file("createPDF.js")
     if args.assembly:
