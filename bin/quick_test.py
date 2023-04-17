62dfe160ab6cad60130797d4f267b71e21c9ffde
MBHallgren <s153002@student.dtu.dk>
2023-04-17 13:44:39 +0200

    virus9

--- bin/quick_test.py
+++ bin/quick_test.py
@@ -1,6 +1,8 @@
 import os
 import sys
 
+import argparse
+
 sys.path = [os.path.join(os.path.dirname(os.path.realpath(__file__)),'..')] + sys.path
 
 import src.sqlCommands as sqlCommands
@@ -16,7 +18,20 @@ def clean_up(md5_list):
         sqlCommands.sql_execute_command('DELETE FROM sequence_table WHERE entry_id = \"{}\"'.format(item), '/opt/LPF_databases/LPF.db')
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Quick test script')
+    parser.add_argument('-virus', action='store_true', help='Run virus test')
+    parser.add_argument('-bacteria', action='store_true', help='Run bacteria test')
+
+    args = parser.parse_args()
+
+
     md5_list = ['62b06be200d3967db6b0f6023d7b5b2e', 'fac82762aa980d285edbbcd45ce952fb', '83d1531bdc862f7ddbf754221fae6a66']
     clean_up(md5_list)
+    if args.virus:
+        os.system('python3 /opt/LPF/src/batchStarter.py -analysis_type virus -batch_json /opt/LPF/tests/fixtures/virus_batch.json')
+        sys.exit()
+    if args.bacteria:
+        os.system('python3 /opt/LPF/src/batchStarter.py -analysis_type bacteria -batch_json /opt/LPF/tests/fixtures/bacteria_batch.json')
+        sys.exit()
     os.system('python3 /opt/LPF/src/batchStarter.py -analysis_type bacteria -batch_json /opt/LPF/tests/fixtures/bacteria_batch.json')
     os.system('python3 /opt/LPF/src/batchStarter.py -analysis_type virus -batch_json /opt/LPF/tests/fixtures/virus_batch.json')
