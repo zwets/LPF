0ffed031186c88de8f0ae731a45467535d6ac25c
MBHallgren <s153002@student.dtu.dk>
2023-04-18 09:24:51 +0200

    virus_32

--- src/localPhylogeny.py
+++ src/localPhylogeny.py
@@ -1,7 +1,10 @@
 import os
 import sys
+import random
 
-def local_phylogeny_from_input_and_database(input, database, specie):
+import src.util.kmaUtils as kmaUtils
+
+def local_phylogeny_from_input_and_database(input, database, specie, target_dir):
     """Runs local phylogeny from input and database"""
     reference_species = list()
     with open(database + ".name", 'r') as f:
@@ -10,3 +13,12 @@ def local_phylogeny_from_input_and_database(input, database, specie):
                 reference_species.append(line.rstrip().split(' ')[0])
     print (reference_species)
     print (len(reference_species))
+    if len(reference_species) == 0:
+        return None
+    if len(reference_species) > 20:
+        reference_species = random.sample(reference_species, 20)
+    os.mkdir(target_dir + "/local_phylogeny")
+    for item in reference_species:
+        template_number = kmaUtils.findTemplateNumber(item, database)
+        os.system('kma seq2fasta -t_db {} -seqs {} > {}/local_phylogeny/{}.fasta'.format(database, template_number, target_dir, item))
+
