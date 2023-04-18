7f8e38988cfda4cf2a7e4ad6e641950102b97a15
MBHallgren <s153002@student.dtu.dk>
2023-04-18 09:37:13 +0200

    virus_36

--- src/localPhylogeny.py
+++ src/localPhylogeny.py
@@ -17,8 +17,10 @@ def local_phylogeny_from_input_and_database(input, database, specie, target_dir)
         return None
     if len(reference_species) > 20:
         reference_species = random.sample(reference_species, 20)
+    print (reference_species)
     os.mkdir(target_dir + "/local_phylogeny")
     for item in reference_species:
+        print (item, database)
         template_number = kmaUtils.findTemplateNumber(item, database)
         print (template_number)
         print ('kma seq2fasta -t_db {} -seqs {} > {}/local_phylogeny/{}.fasta'.format(database, template_number, target_dir, item.split(':')[0]))
